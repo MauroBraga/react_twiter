4ce35aae9ed65fd9ee0c2c71fbf0579f201be511
= <mrb0305@gmail.com>
2020-08-10 02:12:38 -0300

    backend finalizado

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+
+const app = express();
+
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+module.exports = mongoose.connect('mongodb://localhost/goweek-backend', { useNewUrlParser: true })
+
+app.use((req, res,next) =>{
+    req.io = io;
+    return next();
+})
+
+app.use(express.json())
+app.use(require('./routes'))
+
+server.listen(3000,()=>{console.log(' : ) Server started on port 3000')});
